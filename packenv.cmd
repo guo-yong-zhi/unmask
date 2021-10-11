ef1a32715b718f40a19f6ebce8029f8ba472d230
guo-yong-zhi <guo-yong-zhi@outlook.com>
2021-10-11 05:42:09 -0400

    change data path

--- packenv.cmd
+++ packenv.cmd
@@ -2,9 +2,13 @@ conda env create -f environment.yml
 conda pack -n unmask-environment
 mkdir -p unmask-environment
 tar -xzf unmask-environment.tar.gz -C unmask-environment
+del unmask-environment.tar.gz
 
-set NLTK_DATA=nltk_data
-set TRANSFORMERS_CACHE=transformers_data
+set NLTK_DATA=data\nltk_data
+set TRANSFORMERS_CACHE=data\transformers_data
 python unmask.py
+del data\nltk_data\taggers\averaged_perceptron_tagger.zip
+del data\nltk_data\taggers\universal_tagset.zip
+del data\nltk_data\tokenizers\punkt.zip
 
 pause
--- start-up.cmd
+++ start-up.cmd
@@ -1,8 +1,8 @@
 call .\unmask-environment\Scripts\activate.bat
 call .\unmask-environment\Scripts\conda-unpack.exe
 
-set NLTK_DATA=nltk_data
-set TRANSFORMERS_CACHE=transformers_data
+set NLTK_DATA=data\nltk_data
+set TRANSFORMERS_CACHE=data\transformers_data
 voila UnmaskApp.ipynb
 
 pause
