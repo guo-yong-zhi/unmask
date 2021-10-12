499b932f19c0bc023200d502bf185ab3a2b48920
guo-yong-zhi <55872791+guo-yong-zhi@users.noreply.github.com>
2021-10-12 20:51:35 +0800

    echo Installation failed

--- start-up.cmd
+++ start-up.cmd
@@ -1,8 +1,12 @@
+@echo off
 if not exist unmask-environment (
     echo Installing...
     mkdir unmask-environment
     tar -xzf unmask-environment.tar.gz -C unmask-environment
+    if errorlevel 1 echo Installation failed. Please manually unpack the file unmask-environment.tar.gz to unmask-environment folder.
     )
+echo on
+
 call .\unmask-environment\Scripts\activate.bat
 call .\unmask-environment\Scripts\conda-unpack.exe
 
