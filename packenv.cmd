61136418fd3d0119b8af6394bef04b236db175ca
guo-yong-zhi <55872791+guo-yong-zhi@users.noreply.github.com>
2023-09-19 00:54:11 +0800

    update packenv

--- packenv.cmd
+++ packenv.cmd
@@ -1,9 +1,10 @@
 conda env create -f environment.yml
-conda pack -n unmask-environment
+conda pack -n holly-environment
 
 set NLTK_DATA=data\nltk_data
 set TRANSFORMERS_CACHE=data\transformers_data
-python unmask.py
+python src/unmask.py
+python src/check_word.py
 del data\nltk_data\taggers\averaged_perceptron_tagger.zip
 del data\nltk_data\taggers\universal_tagset.zip
 del data\nltk_data\tokenizers\punkt.zip
